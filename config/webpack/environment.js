1a14cd7ca31347a0edbf8a8164d3b8dd0c846057
S8799 <nagano.masami@jgc.com>
2021-10-04 10:38:33 +0900

    edit

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,9 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
+const extractCSS = false
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 module.exports = environment
-environment.loaders.get('vue').options.extractCSS = false
+//environment.loaders.get('vue').options.extractCSS = false
